8b7c4d1a3473898523f1e748d7771843ddc9724a
Kelsey Low <kklowlow@gmail.com>
2019-01-08 02:08:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |string| 
+    string[0] == "r" 
+  end
+end
+
+def contain_a(array)
+    array.select do |string|
+      string.include?("a")
+  end
+end
+
+def first_wa(array)
+    array.detect do |string|
+    string[0,2] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |value|
+    value.is_a?(String) == false 
+  end
+end
+
+def count_elements(array)
+  
+end
+
+def merge_data(keys, data)
+  
+end
